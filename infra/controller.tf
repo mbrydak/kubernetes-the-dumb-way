3b8aa232aa02040c2f3c0e36bec1f2f515051fee
Max Brydak <maxbrydak@gmail.com>
2022-10-02 18:57:51 +0200

    cert generation update; compute instances networking configuration fix, github-actions oidc federation proof of concept

--- infra/controller.tf
+++ infra/controller.tf
@@ -6,6 +6,7 @@ resource "google_compute_instance" "controller_node" {
   network_interface {
     subnetwork = google_compute_subnetwork.kubernetes.name
     network_ip = "10.240.0.${count.index + 10}"
+    access_config {}
   }
   can_ip_forward = true
   boot_disk {
@@ -13,6 +14,10 @@ resource "google_compute_instance" "controller_node" {
       image = var.image
     }
   }
+  # metadata_startup_script = base64encode(templatefile("${path.module}/startup.sh", {
+  #   hostname = "controller-${count.index}"
+  #   ip       = "10.240.0.${count.index + 10}"
+  # }))
   service_account {
     scopes = var.controller_service_account_scope
   }
